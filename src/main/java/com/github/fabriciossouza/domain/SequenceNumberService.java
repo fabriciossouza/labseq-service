35a733e075c2090df5073227cb77f0f8dc98616e
Fabricio <fsouza@Novo.lan>
2023-04-27 10:40:48 +0100

    implement SequenceNumberService

--- src/main/java/com/github/fabriciossouza/domain/SequenceNumberService.java
+++ src/main/java/com/github/fabriciossouza/domain/SequenceNumberService.java
@@ -1,4 +1,33 @@
 package com.github.fabriciossouza.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SequenceNumberService {
+
+    private Map<Integer, Long> memo;
+
+    public SequenceNumberService() {
+        memo = new HashMap<>();
+        memo.put(0, 0L);
+        memo.put(1, 1L);
+        memo.put(2, 0L);
+        memo.put(3, 1L);
+    }
+
+    public long get(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be non-negative");
+        }
+
+        if (!memo.containsKey(n)) {
+            for (int i = 4; i <= n; i++) {
+                if (!memo.containsKey(i)) {
+                    long result = memo.get(i - 4) + memo.get(i - 3);
+                    memo.put(i, result);
+                }
+            }
+        }
+        return memo.get(n);
+    }
 }
